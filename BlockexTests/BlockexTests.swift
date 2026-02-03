4bf39590b0c05aa58bbf9c98fb429f3b42b80072
Mehul112004 <mehulbohra11@gmail.com>
2026-02-03 17:08:14 +0530

    Initial Commit

---
+++ BlockexTests/BlockexTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlockexTests.swift
+//  BlockexTests
+//
+//  Created by Mehul Sharma on 03/02/26.
+//
+
+import Testing
+@testable import Blockex
+
+struct BlockexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ BlockexUITests/BlockexUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BlockexUITestsLaunchTests.swift
+//  BlockexUITests
+//
+//  Created by Mehul Sharma on 03/02/26.
+//
+
+import XCTest
+
+final class BlockexUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
